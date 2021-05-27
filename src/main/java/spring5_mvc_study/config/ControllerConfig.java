b4d5a6da42b0a7ac27b9ecce0f0b75f3560fdfbc
MyungHunSong <change1677@naver.com>
2021-05-27 09:39:16 +0900

    Json 추가

---
+++ src/main/java/spring5_mvc_study/config/ControllerConfig.java
@@ -0,0 +1,17 @@
+package spring5_mvc_study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import spring5_mvc_study.controller.HelloController;
+
+@Configuration
+@ComponentScan(basePackages = {"spring5_mvc_study.controller"})
+public class ControllerConfig {
+	
+	@Bean
+	public HelloController helloController() {
+		return new HelloController();
+	}
+}
